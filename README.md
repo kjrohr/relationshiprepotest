6349a27859c7fb2eb43239c4c71416ed00a6f80e
Karl Rohr <kjrohr@fullsail.edu>
2016-09-25 21:46:08 -0400

    Done a bit of work, followed some tutorials in the symfony documentation, we're thinking about switching to a LAMP stack

---
+++ README.md
@@ -0,0 +1,4 @@
+relationshipRepoTest
+====================
+
+A Symfony project created on September 25, 2016, 7:33 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ));
+    }
+
+    /**
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function adminAction()
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/dashboard.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ));
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+          'last_username' => $lastUsername,
+          'error'         => $error,
+        ));
+    }
+}
